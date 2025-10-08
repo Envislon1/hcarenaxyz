0d42420689e87f06035e1c3664005f020fde0bfe
Envislon1 <gracergysolary.ng@gmail.com>
2025-10-08 23:58:51 +0100

    b

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/hcarenaxy/",
   server: {
     host: "::",
     port: 8080,
