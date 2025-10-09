b775157d988ed13c4279ceb65d8fb5e64e0987ee
Envislon1 <gracergysolary.ng@gmail.com>
2025-10-09 02:41:26 +0100

    g

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/context/AuthContext";
 import { VersionUpdatePrompt } from "@/components/VersionUpdatePrompt";
 import { useVersionCheck } from "@/hooks/useVersionCheck";
@@ -42,7 +42,7 @@ const AppContent = () => {
           onDismiss={dismissUpdate}
         />
       )}
-      <BrowserRouter>
+      <Router>
           <Routes>
             <Route element={<Layout />}>
               <Route path="/" element={<Index />} />
@@ -62,7 +62,7 @@ const AppContent = () => {
             </Route>
             <Route path="*" element={<NotFound />} />
           </Routes>
-      </BrowserRouter>
+      </Router>
     </>
   );
 };
